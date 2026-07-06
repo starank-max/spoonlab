c148529fbb62ec6f5923b3c8083719c72b88c067
starank-max <starank@gmail.com>
2026-07-07 04:45:02 +0800

    chore: remove test page, deployment verified

--- spoonlab/src/app/[locale]/test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: 40, fontFamily: "system-ui" }}>
-      <h1>SpoonLab is running!</h1>
-      <p>If you see this, the deployment works.</p>
-    </div>
-  );
-}
